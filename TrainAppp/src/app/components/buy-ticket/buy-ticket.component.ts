e4852524fe64a919b9a08adafd7d0a47d3d6b603
DanieleCanino <124039023+DanieleCanino@users.noreply.github.com>
2023-04-02 19:55:38 +0200

    Create buy-ticket.component.ts

---
+++ TrainAppp/src/app/components/buy-ticket/buy-ticket.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-buy-ticket',
+  templateUrl: './buy-ticket.component.html',
+  styleUrls: ['./buy-ticket.component.css']
+})
+export class BuyTicketComponent {
+
+}
+
