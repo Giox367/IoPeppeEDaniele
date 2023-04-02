e4c91a58372b40a8c95ecd3830ac837ecb4bc251
DanieleCanino <124039023+DanieleCanino@users.noreply.github.com>
2023-04-02 16:36:09 +0200

    Update navbar.component.ts

--- TrainAppp/src/app/components/navbar/navbar.component.ts
+++ TrainAppp/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
 
 @Component({
+  selector: 'app-nav-bar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+
+export class NavBarComponent implements OnInit{
+  name ?: string
+  constructor(private route : Router){}
+  ngOnInit(): void {
+
+  }
+  logout()
+  {
+    localStorage.clear();
+    this.route.navigate([''])
+  }
 
 }
