8a44804f544c48dcbdc8da24a35126b310712c15
DanieleCanino <124039023+DanieleCanino@users.noreply.github.com>
2023-04-18 18:49:14 +0200

    fix

--- TrainAppp/src/app/app.module.ts
+++ TrainAppp/src/app/app.module.ts
@@ -3,41 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from '../app/components/navbar/navbar.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { BuyTicketComponent } from './components/buy-ticket/buy-ticket.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgForm } from '@angular/forms';
-
-
-
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent,
-    HomePageComponent,
-    BuyTicketComponent,
-
-
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NoopAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
-
-  ],
-  exports:[
-    FormsModule,
-    ReactiveFormsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  exports: [],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
