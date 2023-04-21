08e1fe133c37b6efd857109b49c6eb4abf478648
DanieleCanino <124039023+DanieleCanino@users.noreply.github.com>
2023-04-21 15:09:28 +0200

    update all home

--- TrainAppp/src/app/components/login/login-routing-module.ts
+++ TrainAppp/src/app/components/login/login-routing-module.ts
@@ -5,11 +5,7 @@ import { LoginComponent } from './login.component';
 
 
 const routes: Routes = [
-  {path:'' , component: LoginComponent},
-
-
-
-
+  {path:'' , pathMatch:"full", component: LoginComponent},
 ];
 
 @NgModule({
