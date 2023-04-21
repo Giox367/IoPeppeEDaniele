633b4402901d41971a7c17a87761eea299dd0403
DanieleCanino <124039023+DanieleCanino@users.noreply.github.com>
2023-04-21 10:29:35 +0200

    Update app-routing.module.ts

--- TrainAppp/src/app/app-routing.module.ts
+++ TrainAppp/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
+  {path:'home' , pathMatch:"full", component: HomeComponent},
+    {path:"" , pathMatch:"full", component: LoginComponent}
+
 
 ];
 
