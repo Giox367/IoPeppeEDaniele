4e4d5ebe019307a36fd3d74d69aa06af2529803f
DanieleCanino <124039023+DanieleCanino@users.noreply.github.com>
2023-04-26 12:47:50 +0200

    Update app-routing.module.ts

--- TrainAppp/src/app/app-routing.module.ts
+++ TrainAppp/src/app/app-routing.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
-  {path:'home' , pathMatch:"full", component: HomeComponent},
-    {path:"" , pathMatch:"full", component: LoginComponent},
-
-
-
-
+  { path: '', pathMatch: 'full', component: LoginComponent },
+  { path: 'home', pathMatch: 'full', component: HomeComponent },
+  { path: 'about', pathMatch: 'full', component: AboutComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
